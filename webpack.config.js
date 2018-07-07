96eeb1d47530650348222d413ee85a9f0913ad9c
Alberto Valverde <alberto@toscat.net>
2018-07-07 14:53:49 +0200

    removed in-source build artifacts and configured bs not to build in-source

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const outputDir = path.join(__dirname, "build/");
+const srcDir = path.join(__dirname, "lib/js/src");
 
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  entry: './src/Index.bs.js',
+  entry: path.join(srcDir, 'Index.bs.js'),
   mode: isProd ? 'production' : 'development',
   output: {
     path: outputDir,
