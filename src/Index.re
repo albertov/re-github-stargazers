a0740037d3bae2a0167a3c0d955bb2b6fd46bcd5
Alberto Valverde <alberto@toscat.net>
2018-07-07 22:12:06 +0200

    sass, html and file webpack loaders. Integrated semantic-ui-sass

--- src/Index.re
+++ src/Index.re
@@ -1,3 +1,4 @@
-ReactDOMRe.renderToElementWithId(<Component1 message="Hello 1!" />, "index1");
+[%%raw "require('style/index.scss')"];
 
+ReactDOMRe.renderToElementWithId(<Component1 message="Hello 1!" />, "index1");
 ReactDOMRe.renderToElementWithId(<Component2 greeting="Hello 2!" />, "index2");
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,63 @@
-const path = require('path');
-const outputDir = path.join(__dirname, "build/");
-const srcDir = path.join(__dirname, "lib/js/src");
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
+const path = require('path');
+const outputDir = path.join(__dirname, "build");
+const srcDir = path.join(__dirname, "src");
+const generatedSrcDir = path.join(__dirname, "lib/js/src");
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  entry: path.join(srcDir, 'Index.bs.js'),
+  entry: path.join(generatedSrcDir, 'Index.bs.js'),
   mode: isProd ? 'production' : 'development',
   output: {
     path: outputDir,
-    publicPath: outputDir,
-    filename: 'Index.js',
+    publicPath: '',
+    filename: 'index.js',
   },
+  resolve: {
+    alias: {
+      style: path.join(srcDir, "style")
+    }
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      debug: !isProd
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(srcDir, "index.html"),
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: { discardComments: { removeAll: true } },
+      canPrint: true
+    })
+  ],
+  module: {
+    rules: [
+      { test: /\.scss$/,
+        use: [
+          isProd? MiniCssExtractPlugin.loader : "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=fonts/[hash]-[name].[ext]'
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|webp|svg)$/,
+        loader: 'file-loader?name=images/[hash]-[name].[ext]'
+      },
+    ]
+  }
 };
